13f585ce03fac3eeb78ba28d3a5e44d48b8a4830
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-25 23:58:31 -0400

    add text to Packages section

--- src/components/Packages.js
+++ src/components/Packages.js
@@ -2,6 +2,9 @@ import React from 'react'
 
 export const Packages = props => {
   return (
-    <section className="packages"></section>
+    <section className="packages">
+      <h3 className="title">Tour Packages</h3>
+      <p>We offer a variety of mountaineering packages. Whether you've climbed Everest or don't even know what a mountain is, we've got the perfect vacation for you.</p>
+    </section>
   )
 }
