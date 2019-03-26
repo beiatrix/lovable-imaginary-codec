89d343d44d515f6a7422d3a8d518778931b90a6f
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-26 00:27:08 -0400

    add Testimonials component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Header, Hero, Destinations, Packages} from './components'
+import {Header, Hero, Destinations, Packages, Testimonials} from './components'
 import './styles/App.css';
 
 class App extends Component {
@@ -10,6 +10,7 @@ class App extends Component {
         <Hero />
         <Destinations />
         <Packages />
+        <Testimonials />
       </div>
     );
   }
---
+++ src/components/Testimonials.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Testimonials = props => {
+  return (
+    <section className="testimonials">
+      <h3 className="title">Testimonials from our adventurers:</h3>
+      <hr />
+    </section>
+  )
+}
