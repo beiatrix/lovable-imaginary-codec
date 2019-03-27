3dc06eea42d5cd1afb0a3ce9fbaf13ee68a1fc96
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-26 22:04:51 -0400

    styling - Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import '../styles/Contact.css'
 
 export const Contact = props => {
   return (
-    <section>
+    <section className="contact">
       <h3 class="title">Learn more</h3>
       <p>Want to know about our upcoming mountain-related events, or come to one of our mixers? Just sign up for our mailing list. No spam from us, we promise! Except for the spam we give you to keep up your energy while you're hiking through the mountains. We have tons of that.</p>
       <hr />
