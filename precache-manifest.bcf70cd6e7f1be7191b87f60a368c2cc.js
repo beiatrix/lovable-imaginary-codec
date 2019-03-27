80400d53f05fefc2b3c328eb078071d35a2451ec
Beiatrix Pedrasa <bpedrasa@gmail.com>
2019-03-27 19:14:43 -0400

    Updates

---
+++ precache-manifest.bcf70cd6e7f1be7191b87f60a368c2cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6018a374f6f58daee2a4490ba2854ec",
+    "url": "/lovable-imaginary-codec/static/media/mountain4.f6018a37.jpg"
+  },
+  {
+    "revision": "2c0bf330ec7d1123e4e2ea8d25c12c7d",
+    "url": "/lovable-imaginary-codec/static/media/mountain3.2c0bf330.jpg"
+  },
+  {
+    "revision": "0dc209da42c178aeb0e98e1c04f6b59b",
+    "url": "/lovable-imaginary-codec/static/media/mountain2.0dc209da.jpg"
+  },
+  {
+    "revision": "6a0ad76c3bdc8eed9fe397fa2c619233",
+    "url": "/lovable-imaginary-codec/static/media/mountain1.6a0ad76c.jpg"
+  },
+  {
+    "revision": "3b644ba4ee6b4eaa0ea9f98427c5a2f4",
+    "url": "/lovable-imaginary-codec/static/media/main.3b644ba4.jpg"
+  },
+  {
+    "revision": "13237eed0d83450007ce",
+    "url": "/lovable-imaginary-codec/static/js/runtime~main.561a39e3.js"
+  },
+  {
+    "revision": "cc11714819860eee4936",
+    "url": "/lovable-imaginary-codec/static/js/main.1d0b5140.chunk.js"
+  },
+  {
+    "revision": "e8ffd30a3a62c3252ffa",
+    "url": "/lovable-imaginary-codec/static/js/2.6d0639d4.chunk.js"
+  },
+  {
+    "revision": "cc11714819860eee4936",
+    "url": "/lovable-imaginary-codec/static/css/main.5e6ef109.chunk.css"
+  },
+  {
+    "revision": "ff72657c745e430204d53282ff483aed",
+    "url": "/lovable-imaginary-codec/index.html"
+  }
+];
